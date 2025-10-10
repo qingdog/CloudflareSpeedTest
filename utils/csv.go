1c9a1a1767b03310fdc629f549f59ec421a44021
byJoey <36790372+byJoey@users.noreply.github.com>
2025-10-10 13:17:54 +0800

    Update csv.go

--- utils/csv.go
+++ utils/csv.go
@@ -1,195 +1,163 @@
-package utils
+package task
 
 import (
-	"encoding/csv"
 	"fmt"
-	"log"
 	"net"
-	"os"
+	"sort"
 	"strconv"
+	"sync"
 	"time"
+
+	"github.com/XIU2/CloudflareSpeedTest/utils"
 )
 
 const (
-	defaultOutput         = "result.csv"
-	maxDelay              = 9999 * time.Millisecond
-	minDelay              = 0 * time.Millisecond
-	maxLossRate   float32 = 1.0
+	tcpConnectTimeout = time.Second * 1
+	maxRoutine        = 1000
+	defaultRoutines   = 200
+	defaultPort       = 443
+	defaultPingTimes  = 4
 )
 
 var (
-	InputMaxDelay    = maxDelay
-	InputMinDelay    = minDelay
-	InputMaxLossRate = maxLossRate
-	Output           = defaultOutput
-	PrintNum         = 10
-	Debug            = false // 是否开启调试模式
+	Routines      = defaultRoutines
+	TCPPort   int = defaultPort
+	PingTimes int = defaultPingTimes
 )
 
-// 是否打印测试结果
-func NoPrintResult() bool {
-	return PrintNum == 0
+type Ping struct {
+	wg      *sync.WaitGroup
+	m       *sync.Mutex
+	ips     []*net.IPAddr
+	csv     utils.PingDelaySet
+	control chan bool
+	bar     *utils.Bar
 }
 
-// 是否输出到文件
-func noOutput() bool {
-	return Output == "" || Output == " "
-}
-
-type PingData struct {
-	IP       *net.IPAddr
-	Sended   int
-	Received int
-	Delay    time.Duration
-	Colo     string
-}
-
-type CloudflareIPData struct {
-	*PingData
-	lossRate      float32
-	DownloadSpeed float64
-}
-
-// 计算丢包率
-func (cf *CloudflareIPData) getLossRate() float32 {
-	if cf.lossRate == 0 {
-		pingLost := cf.Sended - cf.Received
-		cf.lossRate = float32(pingLost) / float32(cf.Sended)
+func checkPingDefault() {
+	if Routines <= 0 {
+		Routines = defaultRoutines
 	}
-	return cf.lossRate
+	if TCPPort <= 0 || TCPPort >= 65535 {
+		TCPPort = defaultPort
+	}
+	if PingTimes <= 0 {
+		PingTimes = defaultPingTimes
+	}
 }
 
-func (cf *CloudflareIPData) toString() []string {
-	result := make([]string, 7)
-	result[0] = cf.IP.String()
-	result[1] = strconv.Itoa(cf.Sended)
-	result[2] = strconv.Itoa(cf.Received)
-	result[3] = strconv.FormatFloat(float64(cf.getLossRate()), 'f', 2, 32)
-	result[4] = strconv.FormatFloat(cf.Delay.Seconds()*1000, 'f', 2, 32)
-	result[5] = strconv.FormatFloat(cf.DownloadSpeed/1024/1024, 'f', 2, 32)
-	// 如果 Colo 为空，则使用 "N/A" 表示
-	if cf.Colo == "" {
-		result[6] = "N/A"
+func NewPing() *Ping {
+	checkPingDefault()
+	ips := loadIPRanges()
+	return &Ping{
+		wg:      &sync.WaitGroup{},
+		m:       &sync.Mutex{},
+		ips:     ips,
+		csv:     make(utils.PingDelaySet, 0),
+		control: make(chan bool, Routines),
+		bar:     utils.NewBar(len(ips), "可用:", ""),
+	}
+}
+
+func (p *Ping) Run() utils.PingDelaySet {
+	if len(p.ips) == 0 {
+		return p.csv
+	}
+	if Httping {
+		utils.Cyan.Printf("开始延迟测速（模式：HTTP, 端口：%d, 范围：%v ~ %v ms, 丢包：%.2f)\n", TCPPort, utils.InputMinDelay.Milliseconds(), utils.InputMaxDelay.Milliseconds(), utils.InputMaxLossRate)
 	} else {
-		result[6] = cf.Colo
+		utils.Cyan.Printf("开始延迟测速（模式：TCP, 端口：%d, 范围：%v ~ %v ms, 丢包：%.2f)\n", TCPPort, utils.InputMinDelay.Milliseconds(), utils.InputMaxDelay.Milliseconds(), utils.InputMaxLossRate)
 	}
-	return result
+	for _, ip := range p.ips {
+		p.wg.Add(1)
+		p.control <- false
+		go p.start(ip)
+	}
+	p.wg.Wait()
+	p.bar.Done()
+	sort.Sort(p.csv)
+	return p.csv
 }
 
-func ExportCsv(data []CloudflareIPData) {
-	if noOutput() || len(data) == 0 {
-		return
+func (p *Ping) start(ip *net.IPAddr) {
+	defer p.wg.Done()
+	p.tcpingHandler(ip)
+	<-p.control
+}
+
+// bool connectionSucceed float32 time
+func (p *Ping) tcping(ip *net.IPAddr) (bool, time.Duration) {
+	startTime := time.Now()
+	var fullAddress string
+
+	// 检查是否有端口映射（反代模式）
+	port := TCPPort
+	if mappedPort, exists := PortMapping[ip.String()]; exists {
+		port = mappedPort
 	}
-	fp, err := os.Create(Output)
+
+	if isIPv4(ip.String()) {
+		fullAddress = fmt.Sprintf("%s:%d", ip.String(), port)
+	} else {
+		fullAddress = fmt.Sprintf("[%s]:%d", ip.String(), port)
+	}
+	conn, err := net.DialTimeout("tcp", fullAddress, tcpConnectTimeout)
 	if err != nil {
-		log.Fatalf("创建文件[%s]失败：%v", Output, err)
+		return false, 0
+	}
+	defer conn.Close()
+	duration := time.Since(startTime)
+	return true, duration
+}
+
+// pingReceived pingTotalTime
+func (p *Ping) checkConnection(ip *net.IPAddr) (recv int, totalDelay time.Duration, colo string) {
+	if Httping {
+		recv, totalDelay, colo = p.httping(ip)
 		return
 	}
-	defer fp.Close()
-	w := csv.NewWriter(fp) //创建一个新的写入文件流
-	_ = w.Write([]string{"IP 地址", "已发送", "已接收", "丢包率", "平均延迟", "下载速度(MB/s)", "地区码"})
-	_ = w.WriteAll(convertToString(data))
-	w.Flush()
-}
-
-func convertToString(data []CloudflareIPData) [][]string {
-	result := make([][]string, 0)
-	for _, v := range data {
-		result = append(result, v.toString())
-	}
-	return result
-}
-
-// 延迟丢包排序
-type PingDelaySet []CloudflareIPData
-
-// 延迟条件过滤
-func (s PingDelaySet) FilterDelay() (data PingDelaySet) {
-	if InputMaxDelay > maxDelay || InputMinDelay < minDelay { // 当输入的延迟条件不在默认范围内时，不进行过滤
-		return s
-	}
-	if InputMaxDelay == maxDelay && InputMinDelay == minDelay { // 当输入的延迟条件为默认值时，不进行过滤
-		return s
-	}
-	for _, v := range s {
-		if v.Delay > InputMaxDelay { // 平均延迟上限，延迟大于条件最大值时，后面的数据都不满足条件，直接跳出循环
-			break
+	colo = "" // TCPing 不获取 colo
+	for i := 0; i < PingTimes; i++ {
+		if ok, delay := p.tcping(ip); ok {
+			recv++
+			totalDelay += delay
 		}
-		if v.Delay < InputMinDelay { // 平均延迟下限，延迟小于条件最小值时，不满足条件，跳过
-			continue
-		}
-		data = append(data, v) // 延迟满足条件时，添加到新数组中
 	}
 	return
 }
 
-// 丢包条件过滤
-func (s PingDelaySet) FilterLossRate() (data PingDelaySet) {
-	if InputMaxLossRate >= maxLossRate { // 当输入的丢包条件为默认值时，不进行过滤
-		return s
+func (p *Ping) appendIPData(data *utils.PingData) {
+	p.m.Lock()
+	defer p.m.Unlock()
+	p.csv = append(p.csv, utils.CloudflareIPData{
+		PingData: data,
+	})
+}
+
+// handle tcping
+func (p *Ping) tcpingHandler(ip *net.IPAddr) {
+	recv, totalDlay, colo := p.checkConnection(ip)
+	nowAble := len(p.csv)
+	if recv != 0 {
+		nowAble++
 	}
-	for _, v := range s {
-		if v.getLossRate() > InputMaxLossRate { // 丢包几率上限
-			break
-		}
-		data = append(data, v) // 丢包率满足条件时，添加到新数组中
-	}
-	return
-}
-
-func (s PingDelaySet) Len() int {
-	return len(s)
-}
-func (s PingDelaySet) Less(i, j int) bool {
-	iRate, jRate := s[i].getLossRate(), s[j].getLossRate()
-	if iRate != jRate {
-		return iRate < jRate
-	}
-	return s[i].Delay < s[j].Delay
-}
-func (s PingDelaySet) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-// 下载速度排序
-type DownloadSpeedSet []CloudflareIPData
-
-func (s DownloadSpeedSet) Len() int {
-	return len(s)
-}
-func (s DownloadSpeedSet) Less(i, j int) bool {
-	return s[i].DownloadSpeed > s[j].DownloadSpeed
-}
-func (s DownloadSpeedSet) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func (s DownloadSpeedSet) Print() {
-	if NoPrintResult() {
+	p.bar.Grow(1, strconv.Itoa(nowAble))
+	if recv == 0 {
 		return
 	}
-	if len(s) <= 0 { // IP数组长度(IP数量) 大于 0 时继续
-		fmt.Println("\n[信息] 完整测速结果 IP 数量为 0，跳过输出结果。")
-		return
+	// 获取使用的端口
+	port := TCPPort
+	if mappedPort, exists := PortMapping[ip.String()]; exists {
+		port = mappedPort
 	}
-	dateString := convertToString(s) // 转为多维数组 [][]String
-	if len(dateString) < PrintNum {  // 如果IP数组长度(IP数量) 小于  打印次数，则次数改为IP数量
-		PrintNum = len(dateString)
+
+	data := &utils.PingData{
+		IP:       ip,
+		Sended:   PingTimes,
+		Received: recv,
+		Delay:    totalDlay / time.Duration(recv),
+		Colo:     colo,
+		Port:     port,
 	}
-	headFormat := "%-16s%-5s%-5s%-5s%-6s%-12s%-5s\n"
-	dataFormat := "%-18s%-8s%-8s%-8s%-10s%-16s%-8s\n"
-	for i := 0; i < PrintNum; i++ { // 如果要输出的 IP 中包含 IPv6，那么就需要调整一下间隔
-		if len(dateString[i][0]) > 15 {
-			headFormat = "%-40s%-5s%-5s%-5s%-6s%-12s%-5s\n"
-			dataFormat = "%-42s%-8s%-8s%-8s%-10s%-16s%-8s\n"
-			break
-		}
-	}
-	Cyan.Printf(headFormat, "IP 地址", "已发送", "已接收", "丢包率", "平均延迟", "下载速度(MB/s)", "地区码")
-	for i := 0; i < PrintNum; i++ {
-		fmt.Printf(dataFormat, dateString[i][0], dateString[i][1], dateString[i][2], dateString[i][3], dateString[i][4], dateString[i][5], dateString[i][6])
-	}
-	if !noOutput() {
-		fmt.Printf("\n完整测速结果已写入 %v 文件，可使用记事本/表格软件查看。\n", Output)
-	}
+	p.appendIPData(data)
 }
